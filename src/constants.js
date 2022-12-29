11ad82578485d4c7038038bf7c3276e5b092bbc9
Agustin <agustinsavoy1@gmail.com>
2022-12-29 19:14:31 +0100

    deploy front-end

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://pokemon-backend-dg1m.onrender.com"; // "http://localhost:3001"
 export const POKEMONS_URL = BASE_URL + "/pokemons";
 export const TYPES_URL = BASE_URL + "/types";
